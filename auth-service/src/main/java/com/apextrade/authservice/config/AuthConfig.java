2b96c97e80343a8ea1243ab551ea05d418cf9e35
wahidsaeed <wahidsaeed1@outlook.com>
2026-03-26 11:16:24 +0100

    New Changes

--- auth-service/src/main/java/com/apextrade/authservice/config/AuthConfig.java
+++ auth-service/src/main/java/com/apextrade/authservice/config/AuthConfig.java
@@ -25,7 +25,7 @@ public class AuthConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**", "/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .build();
